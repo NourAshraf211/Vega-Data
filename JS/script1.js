5f266a9f4b2a34d1dc62102717e46cb57c6458ab
Nourhan Ashraf <nourashraf.fcih@gmail.com>
2022-01-27 19:28:23 +0200

    drag list

--- JS/script1.js
+++ JS/script1.js
@@ -1,76 +1,69 @@
-var fills = document.querySelectorAll('img');
+var images = document.querySelectorAll('img');
 var contains = document.querySelectorAll('.drag-contain');
+var chartlist = document.querySelector('.chart-list');
+var places = document.querySelectorAll('.place');
 
-
-for(var fill of fills){
-    fill.addEventListener('dragstart', dragStart);
-    fill.addEventListener('dragend', dragEnd);
+// Events
+for (var image of images) {
+    image.addEventListener('dragstart', dragStart);
+    image.addEventListener('dragend', dragEnd);
 }
 
-for(const contain of contains){
-    contain.addEventListener('dragover' , dragOver);
-    contain.addEventListener('dragenter' , dragEnter);
-    contain.addEventListener('dragleave' , dragLeave);
-    contain.addEventListener('drop' , dragDrop);
+for (const contain of contains) {
+    contain.addEventListener('dragover', dragOver);
+    contain.addEventListener('dragenter', dragEnter);
+    contain.addEventListener('dragleave', dragLeave);
+    contain.addEventListener('drop', dragDrop);
 }
 
+chartlist.addEventListener('dragover', dragOver);
+chartlist.addEventListener('dragenter', dragEnter);
+chartlist.addEventListener('dragleave', dragLeave);
+chartlist.addEventListener('drop', dragDrop);
 
 
 //Drag functions
-function dragStart(e){
-    //console.log('start');
+function dragStart(e) {
     this.className += ' opacity';
     //setTimeout(() => (this.className += ' opacity') , 0);
     it = e.target;
-    //console.log(e.target);
 }
 
-function dragEnd(){
-    //console.log('end');
+function dragEnd() {
     this.className = '';
 }
 
-function dragOver(e){
+function dragOver(e) {
     e.preventDefault();
-    //console.log('over');
 }
-function dragEnter(e){
+
+function dragEnter(e) {
     e.preventDefault();
     this.className += ' hovered'
-    //console.log('enter');
 }
-function dragLeave(){
-    this.className = 'drag-contain';
-    //console.log('leave');
+
+function dragLeave() {
+    this.classList.remove('hovered');
 }
-function dragDrop(){
-    this.className = 'drag-contain';
-    //console.log(this.hasChildNodes);
-    if(!this.hasChildNodes()){
-        this.append(it);
-    }
-    else{
-        //console.log('no');
-        for(var list of document.querySelectorAll('.place')){
-            //console.log(list.childNodes.length);
-            //console.log(list.childElementCount);
-            //console.log(list.childNodes.length);
-            if(list.childElementCount == 0){
-                console.log('yess');
-                console.log(list);
-                console.log(this.firstChild);
-                list.append(this.firstChild);
-                this.append(it);
-                //console.log(list.innerHTML);
+
+function dragDrop() {
+    this.classList.remove('hovered');
+    if (this.className == 'drag-contain') {
+        if (!this.hasChildNodes()) {
+            this.append(it);
+        } else {
+            for (var list of places)
+                if (list.childElementCount == 0) {
+                    list.append(this.firstChild);
+                    this.append(it);
+                    break;
+                }
+        }
+    } else if (this.className == 'chart-list') {
+        for (var list of places)
+            if (list.childElementCount == 0) {
+                list.append(it);
                 break;
             }
-            else{
-                console.log('noooo');
-            }
-        }
-        /*if(document.querySelector('.c'))
-        document.querySelector('.chart-list').prepend (this.firstChild);
-        this.append(it);*/
     }
-    //console.log('drop');
 }
